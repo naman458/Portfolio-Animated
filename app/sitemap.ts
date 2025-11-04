96575563922eff52e9e95af359d0c1d94ee37233
Naman Maheshwari <naman.mah458@gmail.com>
2025-11-04 08:55:18 +0530

    Update sitemap URL to new portfolio link

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://me.toinfinite.dev',
+            url: 'https://portfolio-animated-delta.vercel.app',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 1,
