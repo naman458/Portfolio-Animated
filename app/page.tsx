2dba4bfc376af6a31dabf20f58687bb07cff2248
Naman Maheshwari <naman.mah458@gmail.com>
2025-11-04 09:28:52 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import AboutMe from './_components/AboutMe';
 import Banner from './_components/Banner';
-import Experiences from './_components/Experiences';
 import Skills from './_components/Skills';
 import ProjectList from './_components/ProjectList';
 
@@ -10,7 +9,6 @@ export default function Home() {
             <Banner />
             <AboutMe />
             <Skills />
-            <Experiences />
             <ProjectList />
         </div>
     );
